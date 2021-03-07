02034f5d7e2bf36abf7df6b24d3b875af26e895c
HP <HP@NagaChaitanya>
2021-03-07 22:08:49 +0530

    welcome to demo classes

---
+++ github3/src/github3/Welcome2.java
@@ -0,0 +1,13 @@
+package github3;
+
+public class Welcome2 {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to demo classes");
+		System.out.println("welcome to demo classes");
+		System.out.println("welcome to demo classes");
+		System.out.println("welcome to demo classes");
+
+	}
+
+}
