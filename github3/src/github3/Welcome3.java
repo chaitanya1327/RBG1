c5b7669ed2e202741ccce4e7c4c724a871dc7a51
HP <HP@NagaChaitanya>
2021-03-08 11:39:56 +0530

    data is modified

---
+++ github3/src/github3/Welcome3.java
@@ -0,0 +1,22 @@
+package github3;
+
+public class Welcome3 {
+
+	public static void main(String[] args) {
+		System.out.println("we will start the demo");
+		String data1="abc";
+		String data2="xyz" ;
+		if( data1==data2)
+		{
+		System.out.println("data1 and data2 are same");	
+		}
+		else
+		{
+			System.out.println("data1 and data2 are not same");
+		}
+		
+		
+
+	}
+
+}
