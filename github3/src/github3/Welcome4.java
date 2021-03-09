44c19749ba6974f460e4b93c7c176f6530223d06
HP <HP@NagaChaitanya>
2021-03-09 14:15:10 +0530

    compare two strings

---
+++ github3/src/github3/Welcome4.java
@@ -0,0 +1,30 @@
+package github3;
+
+public class Welcome4 {
+
+	public static void main(String[] args) {
+		System.out.println("we will start the demo");
+		String data1="abc";
+		String data2="xyz" ;
+		if( data1==data2)
+		{
+		System.out.println("data1 and data2 are same");	
+		}
+		else
+		{
+			System.out.println("data1 and data2 are not same");
+		}
+		String data3="xyz";
+		if( data2==data3)
+		{
+		System.out.println("data2 and data3 are same");	
+		}
+		else
+		{
+			System.out.println("data2 and data3 are not same");
+		}
+		
+
+	}
+
+}
